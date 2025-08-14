ce7fcfa458d0a033fd6685df6bbb4d1428a56c5f
Prashant <pkargathara7401@gmail.com>
2025-08-14 18:29:40 +0530

    Update CORS

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://auth-app-one-orcin.vercel.app"],
+    origin: "*",
     credentials: true,
   })
 );
